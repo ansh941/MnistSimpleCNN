89e092e06911a201561a96eedb2a8a3c0e6c209f
Sanghyeon An <ansh941@gmail.com>
2020-08-12 15:19:22 +0900

    Add parameter kernel size in ensemble and test

--- code/test.py
+++ code/test.py
@@ -15,9 +15,11 @@ from PIL import Image
 from ema import EMA
 from datasets import MnistDataset
 from transforms import RandomRotation
+from models.modelM3 import ModelM3
 from models.modelM5 import ModelM5
+from models.modelM7 import ModelM7
 
-def run(p_seed=0, p_logdir="temp"):
+def run(p_seed=0, p_kernel_size=5, p_logdir="temp"):
 
     # enable GPU usage ------------------------------------------------------------#
     use_cuda = torch.cuda.is_available()
@@ -31,7 +33,13 @@ def run(p_seed=0, p_logdir="temp"):
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=100, shuffle=False)
 
     # model selection -------------------------------------------------------------#
-    model1 = ModelM5().to(device)
+    if(p_kernel_size == 3):
+        model1 = ModelM3().to(device)
+    elif(p_kernel_size == 5):
+        model1 = ModelM5().to(device)
+    elif(p_kernel_size == 7):
+        model1 = ModelM7().to(device)
+
     model1.load_state_dict(torch.load("../logs/%s/model%03d.pth"%(p_logdir,p_seed)))
 
     model1.eval()
@@ -55,9 +63,11 @@ if __name__ == "__main__":
     p.add_argument("--logdir", default="modelM5")
     p.add_argument("--seed", default=0, type=int)
     p.add_argument("--trials", default=30, type=int)
+    p.add_argument("--kernel_size", default=5, type=int)
     args = p.parse_args()
     for i in range(args.trials):
         run(p_seed = args.seed + i,
+            p_kernel_size = args.kernel_size,
             p_logdir = args.logdir)
 
 
